927d4611889e7595594d922668bd00e5d793d02a
Eric Ames <amesen07@gmai.com>
2018-01-10 16:14:58 -0500

    Now able to hold one item. Needs refactor.

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -48,10 +48,7 @@ class NewVisitorTest( unittest.TestCase):
 
         table = self.browser.find_element_by_id( 'id_list_table')
         rows = table.find_elements_by_tag_name( 'tr')
-        self.assertTrue( 
-            any( row.text == '1: Buy feathers' for row in rows)
-            ,'New to-do item did not appear in table'
-        )
+        self.assertIn( '1: Buy peacock feathers', [row.text for row in rows])
 
 
         # User still has text box for item entry. User enters item ( dye feathers)
