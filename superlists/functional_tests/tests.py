892f312a95d5246bd1cafdf1229f2ddc0d9ce996
Eric Ames <amesen07@gmail.com>
2018-01-15 14:02:53 -0500

    Added new FT for testing multiple users.

--- superlists/functional_tests/tests.py
+++ superlists/functional_tests/tests.py
@@ -37,6 +37,10 @@ class NewVisitorTest( LiveServerTestCase):
                     raise e
                 time.sleep( 0.5)
 
+    def insert_list_item( self ,entry):
+        inputbox = self.browser.find_element_by_id( 'id_new_item')
+        inputbox.send_keys( entry)
+        inputbox.send_keys( Keys.ENTER)
 
 
     def test_start_list_and_retrieve( self):
@@ -59,14 +63,33 @@ class NewVisitorTest( LiveServerTestCase):
 
         #User enters items and hits enter, one-by-one
         for i,item in enumerate( items):
-            inputbox = self.browser.find_element_by_id( 'id_new_item')
-            inputbox.send_keys( item)
-            inputbox.send_keys( Keys.ENTER)
+            self.insert_list_item( item)
             self.wait_and_check_for_row_in_table( '%d: %s' % ( i+1 ,item) ,'id_list_table')
 
 
-        # User checks if list persists, she is given a permanent URL for her list
-        self.fail( 'Finish the test!')
 
-        # User visit unique URL
+    def test_multiple_users_can_start_lists( self):
+        # User E stars a new list
+        self.browser.get( self.live_server_url)
+        self.insert_list_item( 'Buy feathers')
+        self.wait_and_check_for_row_in_table( '1: Buy feathers' ,'id_list_table')
+
+        # User E notices her list has a unique URL
+        e_list_url = self.browser.current_url
+        self.assertRegex( e_list_url ,'/lists/.+')
+
+        # User F opens up the page, without seing E's list
+        self.browser.quit()
+        self.browser = webdriver.Firefox()
+
+        self.browser.get( self.live_server_url)
+        page_text = self.browser.find_element_by_tag_name( 'body').text
+        self.assertNotIn( 'Buy feathers' ,page_text)
+        self.assertNotIn( 'Dye feathers' ,page_text)
+
+        # User F starts a new list, enters a new item
+        self.insert_list_item( 'Buy milk')
+
+        # Users stop
+        
 
